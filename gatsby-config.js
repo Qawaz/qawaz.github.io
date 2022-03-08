b8e84d5cd9ffeaf4ed1fa613d1203cf020c1aa57
Waqas Tahir <wakaztahir@gmail.com>
2022-03-08 17:41:32 +0000

    styles in build release fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-material-ui`,
+        `gatsby-plugin-styled-components`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-plugin-typescript`,
