fcfc6cceb395ad47c4ba84db6084e3719e99fea9
Waqas Tahir <wakaztahir@gmail.com>
2022-05-24 08:19:18 +0100

    update

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages')
 
 ghpages.publish('public', {
-    branch: 'public',
+    branch: 'gh-pages',
     repo: 'https://github.com/codeckle/codeckle.github.io.git'
 }, () => {
     console.log('Deployed "public" to "public" branch in Github')
